9bc3b0eb9da8600e8f69d822be9e484210464e02
noogler-eng <sharadpoddar10102003@gmail.com>
2026-09-06 00:19:13 +0530

    adding serilization libraries

--- dart/http_calls.dart
+++ dart/http_calls.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'dart:io';
 
 void main() {
@@ -7,6 +8,10 @@ void main() {
   String url = 'https://jsonplaceholder.typicode.com/todos/';
   http.get(Uri.parse(url)).then((response) {
     print('Response received');
+    // jsonEncode is used to convert the response body to a json string
+    // jsonDecode is used to convert the response body to a json object
+    print((jsonDecode(response.body))[0]);
+
   
     // making an json file and storing it in the dart folder
     final file = File('./dart/data/todo.json');
@@ -17,9 +22,12 @@ void main() {
       // and writeAsStringSync is used to write the response body 
       // to the file synchronously.
       file.absolute.writeAsStringSync(response.body);
+
     }catch(e){
       print('Error: $e');
     }
+  }).catchError((error) {
+    print('Error: $error');
   });
 
   print('end of main func');
